6a3bae1036aed5950c98369e19bda7201b09057e
Domino731 <d.orzechowski18@gmail.com>
2021-09-02 10:34:19 +0200

    logic - passwordReset class - displaying form

--- app/auth/passwordReset.ts
+++ app/auth/passwordReset.ts
@@ -1,54 +1,76 @@
-import { Register } from './register';
+import { auth } from "../firebase";
 export class PasswordReset {
-    private root: HTMLElement
-    private toClone: {
+    private forms: {
         login: HTMLElement
         register: HTMLElement
         resetPassword: HTMLElement
     }
-    private forms: {
-        login: Node
-        register: Node
-        resetPassword: Node
-    }
     private buttons: {
         show: HTMLElement
+        hide: HTMLElement
+        action: HTMLElement
     }
+    private input : HTMLInputElement
+    private invalid : HTMLElement
     constructor() {
-        this.root = document.querySelector(".auth__container")
-        this.toClone = {
+        this.forms = {
             login: document.getElementById("auth-login"),
             register: document.getElementById("auth-register"),
             resetPassword: document.getElementById("auth-resetPassword")
         }
-        this.forms = {
-            login: this.toClone.login.cloneNode(true),
-            register: this.toClone.register.cloneNode(true),
-            resetPassword: this.toClone.resetPassword.cloneNode(true)
-        }
         this.buttons = {
-            show: document.querySelector("#auth-forgotPassword")
+            show: document.querySelector("#auth-forgotPassword"),
+            hide: document.querySelector("#auth-backToForms"),
+            action: document.querySelector("#btn__resetPassword")
         }
+        this.input = document.querySelector("#resetPassword-input")
+        this.invalid = document.querySelector("#passwordReset-invalidData")
         this.init()
     }
 
-    // hide reset password form
-    hideResetPasswordForm(){
-        this.root.removeChild(this.root.lastElementChild)
+    // when user click on btn show reset form and hide login and register forms
+    hideAuthFormsEvent() {
+        this.buttons.show.addEventListener("click", () => {
+          this.forms.resetPassword.style.display = "block";
+          this.forms.login.style.display = "none";
+          this.forms.register.style.display = "none"
+        });
     }
 
-    // when user click on btn show reset form and hide login and register forms
-    hideForms() {
-        this.buttons.show.addEventListener("click", ()=>{
-            this.root.innerHTML = ""
-            this.root.appendChild(this.forms.resetPassword) 
-    
+    // when user click on btn hide reset form and show login and register forms
+    showFormsEvent() {
+        this.buttons.hide.addEventListener("click", () => {
+          this.forms.resetPassword.style.display = "none";
+          this.forms.login.style.display = "block";
+          this.forms.register.style.display = "block"
+        });
+    }
+
+    removeErrorsEvent(){
+        this.input.addEventListener("keyup", ()=>{
+            this.invalid.innerText = "";
+            this.input.style.borderBottomColor = "#ffcd00";
+        })
+    }
+  
+    authAction(){
+        this.buttons.action.addEventListener("click", (e: Event) => {
+            e.preventDefault();
+             auth.sendPasswordResetEmail(this.input.value)
+             .then(()=>{
+
+             })
+             .catch((err) => {
+                 this.invalid.innerText = "Invalid e-mail"
+                 this.input.style.borderBottomColor = "#e63946"
+             })
         })
     }
     init() {
-        this.hideResetPasswordForm();
-        this.hideForms();
-        console.log(this.buttons.show);
+        this.removeErrorsEvent();
+        this.hideAuthFormsEvent();
+        this.showFormsEvent();
+         this.authAction();
     }
 
 }
