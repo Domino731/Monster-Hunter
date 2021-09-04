d284427c695fde795c4bd7b5c9e4ec40a8d66871
Domino731 <d.orzechowski18@gmail.com>
2021-09-04 18:26:51 +0200

    styles - pets - pet img and title

--- app/views/pets.ts
+++ app/views/pets.ts
@@ -8,7 +8,22 @@ export class Pets {
 
     async render() {
         this.root.innerHTML = `<section class='game__section pets'>
-           
+           <div class='pets__item'>
+             <img class='pets__img' src='./images/pet_cat.png'/>
+             <h2 class='pets__name pets__name-cat'>Cat</h2>
+           </div>
+           <div class='pets__item'>
+             <img class='pets__img' src='./images/pet_scorpion.png'/>
+             <h2 class='pets__name pets__name-scorpion'>Scorpio</h2>
+           </div>
+           <div class='pets__item'>
+             <img class='pets__img' src='./images/pet_cheetah.png'/>
+             <h2 class='pets__name pets__name-cheetah'>Cheetah</h2>
+           </div>
+           <div class='pets__item'>
+             <img class='pets__img' src='./images/pet_dragon.png'/>
+             <h2 class='pets__name pets__name-dragon'>Dragon</h2>
+           </div>
         </section>`;
     }
 
