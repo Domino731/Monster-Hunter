688cdb170bd1e3b6322a262b325cebb7b2cf273e
Domino731 <d.orzechowski18@gmail.com>
2021-09-08 11:54:37 +0200

    styles - friends - filter form

--- app/views/friends.ts
+++ app/views/friends.ts
@@ -20,54 +20,48 @@ export class Friends {
                 <img  src='./images/friends_icon_filter.png' alt='filter' class='friends__settingsIcon'/>
 
 
-                <form class='friends__settingsForm'>
-                  <div class='friends__checkboxWrapper friends__checkboxWrapper-highestLevel'>
-                      <label> 
-                      
-                      From highest level
-                        <input type='checkbox'>
-                        <span></span>
-                        <i class="fas fa-check"></i>
-                      </label>
-                  </div>
+            
 
-                   <div class='friends__checkboxWrapper friends__checkboxWrapper-LowestLevel'>
-                      <label>   
-                      From lowest level
-                        <input type='checkbox'>
-                        <span></span>
-                        <i class="fas fa-check"></i>
-                      </label>
-                  </div>
 
-                 <div class='friends__checkboxWrapper friends__checkboxWrapper-highestGold'>
-                      <label>   
-                      Highest amount of gold
-                        <input type='checkbox'>
-                        <span></span>
-                        <i class="fas fa-check"></i>
-                      </label>
-                  </div>
+                <form class='friends__settingsForm friends__settingsForm-filter'>
+                <div class='friends__checkboxWrapper friends__checkboxWrapper-highestLevel'>
+                    <label> 
+                      Online friends
+                      <input type='checkbox'>
+                      <span></span>
+                      <i class="fas fa-check"></i>
+                    </label>
+                </div>
 
-                  <div class='friends__checkboxWrapper friends__checkboxWrapper-LowestGold'>
-                      <label>   
-                        Lowest amount of gold
-                        <input type='checkbox'>
-                        <span></span>
-                        <i class="fas fa-check"></i>
-                      </label>
-                  </div>
+                 <div class='friends__checkboxWrapper friends__checkboxWrapper-LowestLevel'>
+                    <label>   
+                    Ofline friends
+                      <input type='checkbox'>
+                      <span></span>
+                      <i class="fas fa-check"></i>
+                    </label>
+                </div>
 
-                   <div class='friends__checkboxWrapper friends__checkboxWrapper-timePlayed'>
-                      <label>   
-                       Game time
-                        <input type='checkbox'>
-                        <span></span>
-                        <i class="fas fa-check"></i>
-                      </label>
-                  </div>
-                </form>
+               <div class='friends__checkboxWrapper friends__checkboxWrapper-highestGold'>
+                    <label>   
+                    With a higher level than yours
 
+                      <input type='checkbox'>
+                      <span></span>
+                      <i class="fas fa-check"></i>
+                    </label>
+                </div>
+
+                <div class='friends__checkboxWrapper friends__checkboxWrapper-LowestGold'>
+                    <label>   
+                    With a lower level than yours
+                      <input type='checkbox'>
+                      <span></span>
+                      <i class="fas fa-check"></i>
+                    </label>
+                </div>
+
+              </form>
 
              </div>
            </div>
@@ -78,3 +72,52 @@ export class Friends {
        this.render();
     }
 }
+
+
+// <form class='friends__settingsForm'>
+// <div class='friends__checkboxWrapper friends__checkboxWrapper-highestLevel'>
+//     <label> 
+    
+//     From highest level
+//       <input type='checkbox'>
+//       <span></span>
+//       <i class="fas fa-check"></i>
+//     </label>
+// </div>
+
+//  <div class='friends__checkboxWrapper friends__checkboxWrapper-LowestLevel'>
+//     <label>   
+//     From lowest level
+//       <input type='checkbox'>
+//       <span></span>
+//       <i class="fas fa-check"></i>
+//     </label>
+// </div>
+
+// <div class='friends__checkboxWrapper friends__checkboxWrapper-highestGold'>
+//     <label>   
+//     Highest amount of gold
+//       <input type='checkbox'>
+//       <span></span>
+//       <i class="fas fa-check"></i>
+//     </label>
+// </div>
+
+// <div class='friends__checkboxWrapper friends__checkboxWrapper-LowestGold'>
+//     <label>   
+//       Lowest amount of gold
+//       <input type='checkbox'>
+//       <span></span>
+//       <i class="fas fa-check"></i>
+//     </label>
+// </div>
+
+//  <div class='friends__checkboxWrapper friends__checkboxWrapper-timePlayed'>
+//     <label>   
+//      Game time
+//       <input type='checkbox'>
+//       <span></span>
+//       <i class="fas fa-check"></i>
+//     </label>
+// </div>
+// </form>
