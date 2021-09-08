fbb6f73a6063fd345011e782a93dbe00d1ada27d
Domino731 <d.orzechowski18@gmail.com>
2021-09-08 12:28:08 +0200

    logic - friends - toggling form - sort and filter

--- app/views/friends.ts
+++ app/views/friends.ts
@@ -1,8 +1,20 @@
 export class Friends {
 
     private root: HTMLElement
+    private dom: {
+        sortBtn: HTMLElement,
+        filterBtn: HTMLElement,
+        sortForm: HTMLElement,
+        filterForm: HTMLElement
+    }
     constructor() {
         this.root = document.getElementById("game__view")
+        this.dom = {
+            sortBtn: document.querySelector('#friends_sort_btn'),
+            filterBtn: document.querySelector('#friends_filter_btn'),
+            sortForm: document.querySelector('#friends_sort_form'),
+            filterForm: document.querySelector('#friends_filter_form')
+        }
         this.init();
     }
 
@@ -16,14 +28,63 @@ export class Friends {
              </div>
 
              <div class='friends__settingsWrapper'>
-                <img  src='./images/friends_icon_sort.png' alt='sort' class='friends__settingsIcon'/>
-                <img  src='./images/friends_icon_filter.png' alt='filter' class='friends__settingsIcon'/>
+                <img  src='./images/friends_icon_sort.png' alt='sort' class='friends__settingsIcon' id='friends_sort_btn'/>
+                <img  src='./images/friends_icon_filter.png' alt='filter' class='friends__settingsIcon'  id='friends_filter_btn'/>
 
 
-            
+                <form class='friends__settingsForm' id='friends_sort_form'>
+                <div class='friends__checkboxWrapper friends__checkboxWrapper-highestLevel'>
+                    <label> 
+                    
+                    From highest level
+                      <input type='checkbox'>
+                      <span></span>
+                      <i class="fas fa-check"></i>
+                    </label>
+                </div>
+                
+                 <div class='friends__checkboxWrapper friends__checkboxWrapper-LowestLevel'>
+                    <label>   
+                    From lowest level
+                      <input type='checkbox'>
+                      <span></span>
+                      <i class="fas fa-check"></i>
+                    </label>
+                </div>
+                
+                <div class='friends__checkboxWrapper friends__checkboxWrapper-highestGold'>
+                    <label>   
+                    Highest amount of gold
+                      <input type='checkbox'>
+                      <span></span>
+                      <i class="fas fa-check"></i>
+                    </label>
+                </div>
+                
+                <div class='friends__checkboxWrapper friends__checkboxWrapper-LowestGold'>
+                    <label>   
+                      Lowest amount of gold
+                      <input type='checkbox'>
+                      <span></span>
+                      <i class="fas fa-check"></i>
+                    </label>
+                </div>
+                
+                 <div class='friends__checkboxWrapper friends__checkboxWrapper-timePlayed'>
+                    <label>   
+                     Game time
+                      <input type='checkbox'>
+                      <span></span>
+                      <i class="fas fa-check"></i>
+                    </label>
+                </div>
+                </form>
+                
 
 
-                <form class='friends__settingsForm friends__settingsForm-filter'>
+
+
+                <form class='friends__settingsForm friends__settingsForm-filter' id='friends_filter_form'>
                 <div class='friends__checkboxWrapper friends__checkboxWrapper-highestLevel'>
                     <label> 
                       Online friends
@@ -68,56 +129,49 @@ export class Friends {
         </section>`;
     }
 
+    showFormsEvents() {
+
+        // toogle filter form
+        this.dom.filterBtn.addEventListener("click", () => {
+
+            //hide sort form
+            this.dom.sortForm.style.display = 'none';
+
+            // toogle filter form
+            const flag: boolean = this.dom.filterForm.style.display === "block";
+            flag ? this.dom.filterForm.style.display = "none" : this.dom.filterForm.style.display = "block"
+        });
+
+
+        // toogle sort form
+        this.dom.sortBtn.addEventListener("click", () => {
+
+            //hide filter form
+            this.dom.filterForm.style.display = 'none';
+
+            // toogle sort form
+            const flag: boolean = this.dom.sortForm.style.display === "block";
+            flag ? this.dom.sortForm.style.display = "none" : this.dom.sortForm.style.display = "block"
+        });
+    }
+
+
+    initScripts() {
+        this.showFormsEvents();
+    }
+    getDOMElements() {
+        this.dom = {
+            sortBtn: document.querySelector('#friends_sort_btn'),
+            filterBtn: document.querySelector('#friends_filter_btn'),
+            sortForm: document.querySelector('#friends_sort_form'),
+            filterForm: document.querySelector('#friends_filter_form')
+        }
+    }
     init() {
-       this.render();
+        this.render();
+        this.getDOMElements();
+        this.initScripts();
     }
 }
 
 
-// <form class='friends__settingsForm'>
-// <div class='friends__checkboxWrapper friends__checkboxWrapper-highestLevel'>
-//     <label> 
-    
-//     From highest level
-//       <input type='checkbox'>
-//       <span></span>
-//       <i class="fas fa-check"></i>
-//     </label>
-// </div>
-
-//  <div class='friends__checkboxWrapper friends__checkboxWrapper-LowestLevel'>
-//     <label>   
-//     From lowest level
-//       <input type='checkbox'>
-//       <span></span>
-//       <i class="fas fa-check"></i>
-//     </label>
-// </div>
-
-// <div class='friends__checkboxWrapper friends__checkboxWrapper-highestGold'>
-//     <label>   
-//     Highest amount of gold
-//       <input type='checkbox'>
-//       <span></span>
-//       <i class="fas fa-check"></i>
-//     </label>
-// </div>
-
-// <div class='friends__checkboxWrapper friends__checkboxWrapper-LowestGold'>
-//     <label>   
-//       Lowest amount of gold
-//       <input type='checkbox'>
-//       <span></span>
-//       <i class="fas fa-check"></i>
-//     </label>
-// </div>
-
-//  <div class='friends__checkboxWrapper friends__checkboxWrapper-timePlayed'>
-//     <label>   
-//      Game time
-//       <input type='checkbox'>
-//       <span></span>
-//       <i class="fas fa-check"></i>
-//     </label>
-// </div>
-// </form>
