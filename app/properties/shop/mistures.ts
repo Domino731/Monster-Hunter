08cc52d821839815c701dab9871ffa1e0733c1bd
Domino731 <d.orzechowski18@gmail.com>
2021-09-10 11:57:35 +0200

    properties - potions data array

---
+++ app/properties/shop/mistures.ts
@@ -0,0 +1,72 @@
+import { potion } from '../../types';
+
+// this array contains data about potions - cost, name, src, properties
+const potionsData: potion[] = [
+    {
+        type: 'potion',
+        name: 'Decoction of cactus',
+        description: 'Straight from Texas',
+        src: './images/physicalEndurance.png',
+        initialCost: 5,
+        properties: {
+            physicalEndurance: 10,
+            strength: null,
+            luck: null,
+            defence: null
+        }
+    },
+
+    {
+        type: 'potion',
+        name: 'Kraken blood',
+        description: 'It was not easy',
+        src: './images/strength.png',
+        initialCost: 5,
+        properties: {
+            strength: 10,
+            physicalEndurance: null,
+            luck: null,
+            defence: null
+        }
+    },
+    {
+        type: 'potion',
+        name: 'Magic of the forest',
+        description: 'Good thing the witch was asleep',
+        src: './images/defence.png',
+        initialCost: 5,
+        properties: {
+            defence: 10,
+            physicalEndurance: null,
+            strength: null,
+            luck: null
+        }
+    },
+    {
+        type: 'potion',
+        name: 'Liquid moon powder',
+        description: 'Imported from Area 51, aliens are still looking for it',
+        src: './images/superPotion.png',
+        initialCost: 5,
+        properties: {
+            physicalEndurance: 15,
+            strength: 15,
+            luck: 15,
+            defence: 15
+        }
+    },
+    {
+        type: 'potion',
+        name: 'Mountain rose',
+        description: 'Passed down from generation to generation, and the power is the same.',
+        src: './images/luck.png',
+        initialCost: 5,
+        properties: {
+            luck: 10,
+            physicalEndurance: 0,
+            strength: 0,
+            defence: 0
+
+        }
+    },
+];
--- app/types.ts
+++ app/types.ts
@@ -2,3 +2,16 @@ export interface route{
     path: string,
     view: any
 }
+export interface potion{
+    type: 'potion',
+    src: string,
+    name: string,
+    description?: string,
+    initialCost: number,
+    properties: {
+        strength: number | null,
+        physicalEndurance: number | null,
+        defence: number | null,
+        luck: number | null
+    }
+}
