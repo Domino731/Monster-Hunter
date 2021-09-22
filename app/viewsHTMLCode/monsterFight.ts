3f3e25e541c21f88ebfd4ba59518bf1a03fedfd9
Domino731 <d.orzechowski18@gmail.com>
2021-09-22 15:51:24 +0200

    styles - monsterFight - monster attack animations

--- app/viewsHTMLCode/monsterFight.ts
+++ app/viewsHTMLCode/monsterFight.ts
@@ -48,7 +48,13 @@ export const getMonsterFightHTMLCode = (userStats: FullUserStats, userData: User
 
 
 
-         <div class='fight__characterWrapper'> 
+         <div class='fight__characterWrapper fight__characterWrapper-monster'> 
+
+
+           <div class='monster__explosionWrapper'> 
+              <img src='./images/explosion.png'  class='monster__explosionImg'/>
+           </div>
+
 
             <img class='fight__characterImg' src='${userData.currentMission.monster.src}'/>
            <div class='fight__healthBar'>
@@ -82,3 +88,84 @@ export const getMonsterFightHTMLCode = (userStats: FullUserStats, userData: User
      </section>
     `
 }
+
+// for rwd - sword attack
+/* 
+     <section class='fight'> 
+
+     <div class='fight__container'>   
+
+
+           <div class='fight__weaponWrapper'> 
+              <img src='./images/explosion.png' class='fight__explosion'/>
+              ${weapon !== undefined ? 
+              `<img src='${weapon.src}' class='fight__sword'/>`
+              :
+              `<img src='./images/fist.png' class='fight__sword'/>`
+              }
+           </div>
+
+         <div class='fight__characterWrapper fight__characterWrapper-user'> 
+           <img class='fight__characterImg' src='${userData.portrait}'/>
+           <div class='fight__healthBar'>
+             <div class='fight__healthPoints'>1440 / ${userStats.health}</div>
+             <div class='fight__health'></div>
+           </div>
+
+           <table class='fight__statsTable'>
+              <tbody>
+                <tr> 
+                  <td>Strength</td>
+                  <td>${userStats.strength}</td>
+                </tr>
+                <tr> 
+                  <td>Physical endurance</td>
+                  <td>${userStats.physicalEndurance}</td>
+                </tr>
+                <tr> 
+                  <td>Defence</td>
+                  <td>${userStats.defence}</td>
+                </tr>
+                <tr> 
+                  <td>luck</td>
+                  <td>${userStats.luck}</td>
+                </tr>
+              </tbody>
+           </table>
+         </div>
+
+
+
+         <div class='fight__characterWrapper'> 
+
+            <img class='fight__characterImg' src='${userData.currentMission.monster.src}'/>
+           <div class='fight__healthBar'>
+              <div class='fight__healthPoints'>1440 / ${userData.currentMission.monster.health}</div>
+             <div class='fight__health'></div>
+           </div>
+
+           <table class='fight__statsTable'>
+               <tbody>
+                <tr> 
+                  <td>Strength</td>
+                  <td>${userStats.strength}</td>
+                </tr>
+                <tr> 
+                  <td>Physical endurance</td>
+                  <td>${userStats.physicalEndurance}</td>
+                </tr>
+                <tr> 
+                  <td>Defence</td>
+                  <td>${userStats.defence}</td>
+                </tr>
+                <tr> 
+                  <td>luck</td>
+                  <td>${userStats.luck}</td>
+                </tr>
+              </tbody>
+           </table>
+         </div>
+
+         </div>
+     </section>
+*/
