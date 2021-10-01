ea6f7d809cb1a38c32a102e4d690f3eec048338c
Domino731 <d.orzechowski18@gmail.com>
2021-10-01 13:28:24 +0200

    media - desktopS - blacksmith, profile

--- app/views/blacksmith.ts
+++ app/views/blacksmith.ts
@@ -786,12 +786,12 @@ export class Blacksmith extends View {
       // this.dom.equipmentLabel.labelWrapper.innerHTML = getEquipmentLabel(currentItem);
       // this.dom.equipmentLabel.root.classList.remove('disabled')
       // backpack
-      const equipmentItem =  helmetsData[12]
-      this.dom.backpackLabel.root.className = 'profile__itemSpecs disabled'
-      this.dom.backpackLabel.root.classList.add(`profile__itemSpecs-backpackSlot10`)
-      this.dom.backpackLabel.replaceIcon.src = getEquipmentIconSrc(currentItem.type)
-      this.dom.backpackLabel.labelWrapper.innerHTML = getBlacksmithBackpackLabel(currentItem, equipmentItem);
-      this.dom.backpackLabel.root.classList.remove('disabled')
+      // const equipmentItem =  helmetsData[12]
+      // this.dom.backpackLabel.root.className = 'profile__itemSpecs disabled'
+      // this.dom.backpackLabel.root.classList.add(`profile__itemSpecs-backpackSlot10`)
+      // this.dom.backpackLabel.replaceIcon.src = getEquipmentIconSrc(currentItem.type)
+      // this.dom.backpackLabel.labelWrapper.innerHTML = getBlacksmithBackpackLabel(currentItem, equipmentItem);
+      // this.dom.backpackLabel.root.classList.remove('disabled')
    }
    initScripts() {
       this.setLastVisit();
