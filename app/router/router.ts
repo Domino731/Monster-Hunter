b41c99966b197b5d0c88aaa0b64101d8081b4e50
Domino731 <d.orzechowski18@gmail.com>
2021-10-04 12:03:29 +0200

    media - laptopS - guard

--- app/router/router.ts
+++ app/router/router.ts
@@ -108,7 +108,7 @@ export const initRouter = () => {
     });
 
     // for development
-    const developmentRender = new Friends()
+    const developmentRender = new Guard()
     window.addEventListener("popstate", router);
 };
 
