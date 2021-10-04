62d341ea90a3395b3ec1568b1fa24f6502cb8197
Domino731 <d.orzechowski18@gmail.com>
2021-10-04 13:24:29 +0200

    media - laptopS - blacksmith

--- app/router/router.ts
+++ app/router/router.ts
@@ -108,7 +108,7 @@ export const initRouter = () => {
     });
 
     // for development
-    const developmentRender = new Pets()
+    const developmentRender = new Blacksmith()
     window.addEventListener("popstate", router);
 };
 
