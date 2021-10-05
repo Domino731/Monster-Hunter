85768304a5e8ede200f88fba5111eb0fa3c566d9
Domino731 <d.orzechowski18@gmail.com>
2021-10-05 19:13:32 +0200

    media - ipadPro - profile

--- app/views/profile.ts
+++ app/views/profile.ts
@@ -637,21 +637,21 @@ export class Profile extends View {
         // equipment
         const currentItem = helmetsData[12]
         // this.dom.equipmentLabel.root.classList.add(currentItem.rarity === 'legendary' ? 'profile__itemSpecs-legendary' : 'profile__itemSpecs-common')
-        // this.dom.equipmentLabel.root.classList.add(`profile__itemSpecs-helmet`)
+        // this.dom.equipmentLabel.root.classList.add(`profile__itemSpecs-chestPlate`)
         // this.dom.equipmentLabel.labelWrapper.innerHTML = getEquipmentLabel(currentItem);
         // this.dom.equipmentLabel.root.classList.remove('disabled')
         // backpack
-          const equipmentItem = this.userData.equipmentItems[0]
-          this.dom.backpackLabel.root.className = 'profile__itemSpecs profile__itemSpecs-backpackSlot disabled'
-          this.dom.backpackLabel.root.classList.add(`profile__backpackLabel-${10}`)
-          this.dom.backpackLabel.replaceIcon.src = getEquipmentIconSrc(currentItem.type)
-          this.dom.backpackLabel.labelWrapper.innerHTML = getBlacksmithBackpackLabel(currentItem, equipmentItem);
-          this.dom.backpackLabel.root.classList.remove('disabled')
+        //   const equipmentItem = this.userData.equipmentItems[0]
+        //   this.dom.backpackLabel.root.className = 'profile__itemSpecs profile__itemSpecs-backpackSlot disabled'
+        //   this.dom.backpackLabel.root.classList.add(`profile__backpackLabel-${6}`)
+        //   this.dom.backpackLabel.replaceIcon.src = getEquipmentIconSrc(currentItem.type)
+        //   this.dom.backpackLabel.labelWrapper.innerHTML = getBlacksmithBackpackLabel(currentItem, equipmentItem);
+        //   this.dom.backpackLabel.root.classList.remove('disabled')
         // potions
-        //     const firstPotion = potionsData[0]
-        //     this.dom.equipmentLabel.root.className = 'profile__itemSpecs disabled';
-        //     this.dom.backpackLabel.root.className = 'profile__itemSpecs disabled';
-        //    this.dom.potionLabel.innerHTML = getPotionLabel(firstPotion, 1);
+            const firstPotion = potionsData[4]
+            this.dom.equipmentLabel.root.className = 'profile__itemSpecs disabled';
+            this.dom.backpackLabel.root.className = 'profile__itemSpecs disabled';
+           this.dom.potionLabel.innerHTML = getPotionLabel(firstPotion, 1);
     }
     initScripts() {
         this.labelForBackpackEvent();
