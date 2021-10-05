b99b60ee423bdea41f92b15cc8a4025d01a7bf76
Domino731 <d.orzechowski18@gmail.com>
2021-10-05 16:17:58 +0200

    media - ipadPro - wizard

--- app/router/router.ts
+++ app/router/router.ts
@@ -108,7 +108,7 @@ export const initRouter = () => {
     });
 
     // for development
-    const developmentRender = new Guard()
+    const developmentRender = new Wizard()
     window.addEventListener("popstate", router);
 };
 
