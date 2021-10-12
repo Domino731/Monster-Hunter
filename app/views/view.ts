d446d4a3339e300ab5f934173bef0aefd044a40a
Domino731 <d.orzechowski18@gmail.com>
2021-10-12 15:12:50 +0200

    general - function - function responsible for creating full user stats

--- app/views/view.ts
+++ app/views/view.ts
@@ -9,6 +9,7 @@ import { allBlacksmithMarketItems } from '../properties/shop/allMarketItems';
 import { getRandomMissions } from '../functions/missionGenerator';
 import { monstersData } from '../properties/missions/monsters';
 import { textChangeRangeIsUnchanged } from 'typescript';
+import { getFullUserStats } from '../functions/getFullUserStats';
 
 export class View {
    protected userData: UserData | null
@@ -59,85 +60,7 @@ export class View {
    }
    // function that will set statistics based on equipment, potions and pet
    setHeroStats() {
-      // reset stats
-      const userStats : FullUserStats = {
-         strength: 0,
-         damage: 0,
-         physicalEndurance: 0,
-         health: 0,
-         defence: 0,
-         damageReduce: 0,
-         luck: 0,
-         critical: 0
-      }
-       this.checkPetAndPotions();
-       // by basic stats
-
-       // strenght
-       userStats.strength = this.userData.rawStats.strength;
-       userStats.damage = Math.floor(userStats.strength * 0.7);
-       // luck
-       userStats.luck = this.userData.rawStats.luck;
-       userStats.critical = userStats.critical = Math.floor(this.userStats.luck * 0.3);
-       // pe
-       userStats.physicalEndurance = this.userData.rawStats.physicalEndurance;
-       userStats.health =  Math.floor(userStats.physicalEndurance * 0.8);
-       // defence
-       userStats.defence = this.userData.rawStats.defence;
-       userStats.damageReduce = Math.floor(userStats.defence * 0.5);
-
-
-
-
-      // by equipment
-  
-      this.userData.equipmentItems.forEach(el => {
-         
-         if (el.properties.strength !== null) {
-            userStats.strength += el.properties.strength;
-            userStats.damage += Math.floor(userStats.strength * 0.7);
-         }
-         if (el.properties.luck !== null) {
-            userStats.luck += el.properties.luck;
-            userStats.critical += Math.floor(userStats.luck * 0.3);
-         }
-         if (el.properties.physicalEndurance !== null) {
-            userStats.physicalEndurance = el.properties.physicalEndurance;
-            userStats.health += Math.floor(userStats.physicalEndurance * 0.8);
-         }
-         if (el.properties.defence !== null) {
-            userStats.defence += el.properties.defence;
-            userStats.damageReduce = Math.floor(userStats.defence * 0.5);
-         }
-      });
-
-      // by pet
-      if (this.userData.pet !== null) {
-         const petProps: PetProperties = this.userData.pet.properties
-         if (petProps.strength !== null) {
-            userStats.strength += Math.floor(this.userData.rawStats.strength * (petProps.strength / 100));
-            userStats.damage = Math.floor(userStats.strength * 0.7);
-         }
-         if (petProps.luck !== null) {
-            userStats.luck += Math.floor(this.userData.rawStats.luck * (petProps.luck / 100));
-            userStats.critical = Math.floor(this.userStats.luck * 0.3);
-         }
-         if (petProps.physicalEndurance !== null) {
-            userStats.physicalEndurance += Math.floor(this.userData.rawStats.physicalEndurance * (petProps.physicalEndurance / 100));
-            userStats.health = Math.floor(userStats.physicalEndurance * 0.8);
-         }
-         if (petProps.defence !== null) {
-            userStats.defence += Math.floor(this.userData.rawStats.defence * (petProps.defence / 100));
-            userStats.damageReduce = Math.floor(userStats.defence * 0.5);
-         }
-      }
-
-      // by potions       
-      const firstPotion: ShopItem | undefined = potionsData[potionsData.findIndex(el => this.userData.potions.first)];
-      const secondPotion: ShopItem | undefined = potionsData[potionsData.findIndex(el => this.userData.potions.second)];
-
-      this.userStats = userStats
-   
+    this.userStats = getFullUserStats(this.userData)
    }
 
    // method responsbile for fetching user data, and also this is real time data lisener
