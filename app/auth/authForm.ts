27ed7d7a4ba2de106e8518f7fb4540582473de53
Domino731 <d.orzechowski18@gmail.com>
2021-10-13 15:02:52 +0200

    logic - auth forms - toggling between forms

--- app/auth/authForm.ts
+++ app/auth/authForm.ts
@@ -6,26 +6,35 @@ export abstract class AuthForm {
     protected btn: HTMLElement;
     protected loading: HTMLElement;
     protected invalidData: boolean;
+    protected loginSwitch : HTMLElement;
+    protected registerSwitch: HTMLElement;
+    protected loginContainer: HTMLElement;
+    protected registerContainer: HTMLElement
     protected data: {
-        eMail: string
-        password: string
-        repeatPassword?: string
-        nickname?: string
+        eMail: string;
+        password: string;
+        repeatPassword?: string;
+        nickname?: string;
     };
 
     constructor(root: string) {
-        this.root = document.querySelector(root)
-        this.inputs = this.root.querySelectorAll("input")
-        this.btn = this.root.querySelector(".auth__submitWrapper button")
-        this.loading = this.root.querySelector(".auth__loading")
+        this.root = document.querySelector(root);
+        this.inputs = this.root.querySelectorAll('input');
+        this.btn = this.root.querySelector('.auth__submitWrapper button');
+        this.loading = this.root.querySelector('.auth__loading');
+        this.registerSwitch = document.querySelector('#register-switch');
+        this.loginSwitch = document.querySelector('#login-switch');
+        this.loginContainer = document.querySelector('#auth-login');
+        this.registerContainer = document.querySelector('#auth-register');
         this.data = {
             eMail: "",
             password: "",
             repeatPassword: "",
             nickname: ""
-        }
-        this.invalidData = true
+        };
+        this.invalidData = true;
         this.init();
+        this.toogleForm();
     };
 
 
@@ -72,6 +81,18 @@ export abstract class AuthForm {
             this.invalidData ? this.setErrors() : this.authAction()
         });
     }
+    toogleForm(){
+    
+        this.registerSwitch.addEventListener('click', ()=> {
+            this.loginContainer.style.display = 'block';
+            this.registerContainer.style.display = 'none';
+        });
+        this.loginSwitch.addEventListener('click', ()=> {
+            this.registerContainer.style.display = 'block';
+            this.loginContainer.style.display = 'none';
+        });
+    }
+
     init() {
         this.addInputEvents();
         this.addButtonEvent();
--- app/auth/passwordReset.ts
+++ app/auth/passwordReset.ts
@@ -10,9 +10,9 @@ export class PasswordReset {
         hide: HTMLElement
         action: HTMLElement
     }
-    private input : HTMLInputElement
+    private input: HTMLInputElement
     private notifications: {
-        invalid : HTMLElement
+        invalid: HTMLElement
         correct: HTMLElement
     }
     constructor() {
@@ -28,57 +28,65 @@ export class PasswordReset {
         }
         this.input = document.querySelector("#resetPassword-input")
         this.notifications = {
-          invalid: document.querySelector("#passwordReset-invalidData"),
-          correct: document.querySelector("#passwordReset-correctData")
+            invalid: document.querySelector("#passwordReset-invalidData"),
+            correct: document.querySelector("#passwordReset-correctData")
         }
-        
+
         this.init()
     }
 
     // when user click on btn show reset form and hide login and register forms
     hideAuthFormsEvent() {
         this.buttons.show.addEventListener("click", () => {
-          this.forms.resetPassword.style.display = "block";
-          this.forms.login.style.display = "none";
-          this.forms.register.style.display = "none"
+            this.forms.resetPassword.style.display = "block";
+            this.forms.login.style.display = "none";
+            this.forms.register.style.display = "none"
         });
     }
 
     // when user click on btn hide reset form and show login and register forms
     showFormsEvent() {
         this.buttons.hide.addEventListener("click", () => {
-          this.forms.resetPassword.style.display = "none";
-          this.forms.login.style.display = "block";
-          this.forms.register.style.display = "block"
+            if (window.innerWidth < 1024) {
+                this.forms.resetPassword.style.display = "none";
+                this.forms.login.style.display = "block";
+                this.forms.register.style.display = "none";
+            }
+            else {
+                this.forms.resetPassword.style.display = "none";
+                this.forms.login.style.display = "block";
+                this.forms.register.style.display = "block";
+            }
+
         });
     }
 
-    removeErrorsEvent(){
-        this.input.addEventListener("keyup", ()=>{
+    removeErrorsEvent() {
+        this.input.addEventListener("keyup", () => {
             this.notifications.invalid.innerText = "";
             this.input.style.borderBottomColor = "#ffcd00";
         })
     }
-  
-    authAction(){
+
+    authAction() {
         this.buttons.action.addEventListener("click", (e: Event) => {
             e.preventDefault();
-             auth.sendPasswordResetEmail(this.input.value)
-             .then(()=>{
-                this.notifications.correct.innerText = "Check your inbox for further instructions"
-             })
-             .catch((err) => {
-                 console.error(err)
-                 this.notifications.invalid.innerText = "Invalid e-mail"
-                 this.input.style.borderBottomColor = "#e63946"
-             })
+            auth.sendPasswordResetEmail(this.input.value)
+                .then(() => {
+                    this.notifications.correct.innerText = "Check your inbox for further instructions"
+                })
+                .catch((err) => {
+                    console.error(err)
+                    this.notifications.invalid.innerText = "Invalid e-mail"
+                    this.input.style.borderBottomColor = "#e63946"
+                })
         })
     }
     init() {
         this.removeErrorsEvent();
         this.hideAuthFormsEvent();
         this.showFormsEvent();
-         this.authAction();
+        this.authAction();
     }
 
 }
