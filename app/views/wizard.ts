106f8804c407926aca3470311596f10c1b2a1a80
Domino731 <d.orzechowski18@gmail.com>
2021-10-14 18:16:46 +0200

    improvements - wizard - general

--- app/views/wizard.ts
+++ app/views/wizard.ts
@@ -1,27 +1,30 @@
 import { getWizardHTMLCode } from '../viewsHTMLCode/wizard';
 import { View } from './view';
-import { ShopItem } from '../types';
-import { getBlacksmithItems } from '../functions/getBlacksmithItems';
-import { potionsData } from '../properties/shop/potions';
-import { getRandomShopItem } from '../functions/getRandomShopItem';
-import { allBlacksmithMarketItems } from '../properties/shop/allMarketItems';
 import { getWonItemLabel } from './sub_views/getWonItemLabel';
-import { setItemStats } from '../functions/setItemStats';
 import { updateUserData } from '../firebase/operations';
-import firebase from 'firebase/app';
 import { setCountdown } from '../functions/countdown';
-import { helmetsData } from '../properties/shop/helmets';
+
+// Class responsible for spinning wheel by which user can earn free equipment item, potion or gold
 export class Wizard extends View {
 
     private dom: {
-        spinningWheelItems: NodeListOf<Element> | null
-        spinBtn: HTMLButtonElement | null
-        spinningWheel: HTMLElement | null
-        spinningCircle: HTMLElement | null
-        wonItemLabel: HTMLElement | null
-        actionError: HTMLElement | null
-        actionBar: HTMLElement | null
-        countdown: HTMLElement | null
+        // array with items wrappers need to set available items (setSpinningWheelItems() method) to win in magic wheel
+        spinningWheelItems: NodeListOf<Element> | null;
+        // button on which function responsible for start spinning wheel is applied -  startSpinningEvent() method
+        spinBtn: HTMLButtonElement | null;
+        // container with wheel needed to add to him spinning animation - startSpinningEvent() method
+        spinningWheel: HTMLElement | null;
+        // gray circle in the middle of the spinning wheel, needed to add to him spinning animation - startSpinningEvent() method
+        spinningCircle: HTMLElement | null;
+        // item label which is displaying after spinning wheel end spinning, contains info about won item - startSpinningEvent() method
+        wonItemLabel: HTMLElement | null;
+        // wrapper which is displaying error when user's backpack is full - startSpinningEvent() method
+        actionError: HTMLElement | null;
+        // wrapper which is  containing button responsible for start spinning wheel, when user click on this button, html in this actionBar
+        // is cleaned so user cant spin the wheel , and the countdown is started - startSpinningEvent() method
+        actionBar: HTMLElement | null;
+        // wrapper which is displaying countdown to next magic wheel spin - addWonItem() method
+        countdown: HTMLElement | null;
     }
     constructor() {
         super()
@@ -37,28 +40,28 @@ export class Wizard extends View {
         }
     }
 
-    render() {
-        this.root.innerHTML = getWizardHTMLCode(this.userData);
-    }
-
     // add won item to user's data
     addWonItem() {
-        // remove spin from user account, next spin will be availble at next day
+        // remove spin from user account, next spin will be available at next day
         this.userData.wizardWheelSpin = false;
+
         // show coundown
         this.dom.countdown.parentElement.classList.remove('disabled');
         setCountdown(this.dom.countdown);
         this.dom.actionBar.innerHTML = '';
-        // add won item to user's data and update his profile in firestore
+
+        // check what type of the won ite is and add this item to user's data and update his profile in firestore
+        // user can win: gold, potion, equipment item
         if (this.userData.magicWheel.wonItem.type === 'gold') {
             this.userData.gold += Math.ceil(this.userData.guardPayout * 10);
         }
         else if (this.userData.magicWheel.wonItem.type === 'potion') {
+            // set potion properties -> id and end date
             const end: Date = new Date();
             end.setHours(end.getHours() + (7 * 24));
-            const newPotion : {id: string, end: Date}= {
-                 id: this.userData.magicWheel.wonItem.id,
-                 end: end
+            const newPotion: { id: string, end: Date } = {
+                id: this.userData.magicWheel.wonItem.id,
+                end: end
             }
             if (this.userData.potions.first === null) {
                 this.userData.potions.first = newPotion;
@@ -70,59 +73,57 @@ export class Wizard extends View {
         else {
             this.userData.backpackItems.push(this.userData.magicWheel.wonItem);
         }
-        updateUserData(this.userData)
+        updateUserData(this.userData);
     }
-    startSpinningEvent() {
-       
-        this.dom.spinBtn.addEventListener('click', () => {
 
-            // check if user's backpack has availble slots
+    // event applied on spinBtn by which user can spin the wheel
+    startSpinningEvent() {
+        this.dom.spinBtn.addEventListener('click', () => {
+            // check if user's backpack has available slots
             if (this.userData.backpackItems.length < 10) {
+
+                // add animation
                 this.dom.spinningWheel.classList.add('spinningWheel__content-animation');
+
                 // spin the wheel
                 setTimeout(() => {
                     this.dom.spinningCircle.classList.add('circle-animation');
                 }, 7000);
+
                 // remove spinning animation and show won item
                 setTimeout(() => {
                     this.dom.spinningWheel.classList.remove('spinningWheel__content-animation');
                     this.dom.wonItemLabel.innerHTML = getWonItemLabel(this.userData.magicWheel.wonItem);
                     this.dom.wonItemLabel.classList.remove('disabled');
                 }, 10000);
+
                 // scroll to spinning wheel (mainly for mobile devices)
-                const wheel : HTMLElement  =  this.dom.spinningWheel.parentElement.parentElement;
-                wheel.scrollTop =  wheel.scrollHeight;
+                const wheel: HTMLElement = this.dom.spinningWheel.parentElement.parentElement;
+                wheel.scrollTop = wheel.scrollHeight;
+
+                // add won item to user's account
                 this.addWonItem();
+
             }
             else {
                 this.dom.actionError.innerText = 'Your backpack is full';
+                // remove this error after 3s
                 setTimeout(() => {
                     this.dom.actionError.innerText = '';
                 }, 3000);
             }
         });
     }
+
+    // set graphics of available items to win
     setSpinningWheelItems() {
         this.userData.magicWheel.items.forEach((el, num) => {
-            this.dom.spinningWheelItems[num].innerHTML = `<img src='${el.src}' class='spinningWheel__itemImg'/>`
+            this.dom.spinningWheelItems[num].innerHTML = `<img src='${el.src}' class='spinningWheel__itemImg'/>`;
         });
     }
 
-    rwd(){
-        const foo = helmetsData[12]
-       // this.dom.spinningWheel.classList.add('spinningWheel__content-animation');
-        // spin the wheel
-      //  setTimeout(() => {
-       //     this.dom.spinningCircle.classList.add('circle-animation');
-      //  }, 7000);
-        // remove spinning animation and show won item
-       // setTimeout(() => {
-         //   this.dom.spinningWheel.classList.remove('spinningWheel__content-animation');
-          //  this.dom.wonItemLabel.innerHTML = getWonItemLabel(foo);
-        //    this.dom.wonItemLabel.classList.remove('disabled');
-       // }, 10000);
-    }
-    onDataChange() { }
+
+
     getDOMElements() {
         this.dom = {
             spinningWheel: document.querySelector('.spinningWheel__content'),
@@ -136,9 +137,12 @@ export class Wizard extends View {
         }
     }
 
+    render() {
+        this.root.innerHTML = getWizardHTMLCode(this.userData);
+    }
     initScripts() {
-        this.rwd();
         this.setSpinningWheelItems();
+        // check if user has available spin
         if (this.userData.wizardWheelSpin) {
             this.startSpinningEvent();
         }
@@ -147,8 +151,9 @@ export class Wizard extends View {
         }
 
     }
-
-
+    onDataChange() {
+        console.log('Data changed');
+    }
 }
 
 // <a href='https://www.freepik.com/vectors/background'>Background vector created by vectorpouch - www.freepik.com</a>
